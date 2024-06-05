c508f94dae5cf513299e04ef434b2743c3d6d9a4
KAMGA <tetakamgasteve@gmail.com>
2024-06-04 23:39:08 -0400

    initialisation du projet partie client avec react

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const App = () => {
+    return (
+        <div>
+            <h1 className='bg-red-500 font-semibold text-3xl'>App</h1>
+        </div>
+    )
+}
+
+export default App
